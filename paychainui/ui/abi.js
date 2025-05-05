8fcd7ab31cf2bd0e227718a195f06fe1467b27d5
genovapink <130900923+genovapink@users.noreply.github.com>
2025-05-05 12:41:42 +0700

    Delete paychainui/ui directory

--- paychainui/ui/abi.js
+++
@@ -1,5 +0,0 @@
-export const abi = [
-    "function send(address to, uint256 amount) public",
-    "function withdraw() public",
-    "event PaymentSent(address indexed to, uint256 amount)"
-  ];  
--- paychainui/ui/style.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("button").forEach(btn => {
-    btn.addEventListener("click", () => {
-      btn.classList.add("ring", "ring-yellow-400");
-      setTimeout(() => {
-        btn.classList.remove("ring", "ring-yellow-400");
-      }, 200);
-    });
-  });
-  
